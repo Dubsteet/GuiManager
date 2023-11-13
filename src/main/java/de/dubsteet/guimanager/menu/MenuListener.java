348cd1cfa23e5d2d18e33269c9f81daa9e5020ec
Felix Hilbig <FelixHilbig@gmail.com>
2023-11-13 22:04:41 +0100

    Added even more debug

--- src/main/java/de/dubsteet/guimanager/menu/MenuListener.java
+++ src/main/java/de/dubsteet/guimanager/menu/MenuListener.java
@@ -12,13 +12,18 @@ public class MenuListener implements Listener {
     public void onMenuClick(InventoryClickEvent event) {
         System.out.println(event.getInventory().getHolder());
         if (event.getInventory().getHolder() instanceof Menu holder) {
+            System.out.println("§aMenu clicked");
             if (event.getCurrentItem() == null) return;
+            System.out.println("§aItem not null");
             //Checks if clicking is allowed
             if (holder.cancelAllClicks()) {
+                System.out.println("§aClicking not allowed");
                 event.setCancelled(true);
             }
             try {
+                System.out.println("§aHandling menu");
                 holder.handleMenu(event);
+                System.out.println("§aHandled menu");
             } catch (MenuManagerNotSetupException menuManagerNotSetupException) {
                 System.out.println("§4THE MENU MANAGER HAS NOT BEEN CONFIGURED. CALL MENUMANAGER.SETUP()");
             } catch (MenuManagerException menuManagerException) {
