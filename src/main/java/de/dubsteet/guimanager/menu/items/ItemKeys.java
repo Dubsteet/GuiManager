620408a33a3712e754401d7b0520b51a352d524c
Felix Hilbig <FelixHilbig@gmail.com>
2024-09-08 11:31:56 +0200

    Fixed NamespacedKeys

--- src/main/java/de/dubsteet/guimanager/menu/items/ItemKeys.java
+++ src/main/java/de/dubsteet/guimanager/menu/items/ItemKeys.java
@@ -4,7 +4,7 @@ import org.bukkit.NamespacedKey;
 
 public class ItemKeys {
 
-    public static final NamespacedKey IS_CLICKABLE = new NamespacedKey("GuiManager", "isclickable");
-    public static final NamespacedKey UNIQUE_ID = new NamespacedKey("GuiManager", "uniqueid");
-    public static final NamespacedKey MENU_FUNCTION = new NamespacedKey("GuiManager", "menufunction");
+    public static final NamespacedKey IS_CLICKABLE = new NamespacedKey("guimanager", "isclickable");
+    public static final NamespacedKey UNIQUE_ID = new NamespacedKey("guimanager", "uniqueid");
+    public static final NamespacedKey MENU_FUNCTION = new NamespacedKey("guimanager", "menufunction");
 }
